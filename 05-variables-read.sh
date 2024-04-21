35aea8c798b3f433de8e29678ea66ed1001bf85a
dudekula999 <dastagiri160689@gmail.com>
2024-04-22 04:58:47 +0530

    demo

--- 05-variables-read.sh
+++ 05-variables-read.sh
@@ -2,7 +2,7 @@
 
 echo "Please enter your username:: "
 
-read -s USERNAME #the value entered above will be automatically atteched to USERNAME variable
+read USERNAME #the value entered above will be automatically atteched to USERNAME variable
 
 echo "Please enter your Password:: "
 read -s PASSWORD
