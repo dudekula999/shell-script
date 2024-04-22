4069877d066d5c5c2db2b930fb9306ea795b45c5
dudekula999 <dastagiri160689@gmail.com>
2024-04-22 07:12:41 +0530

    demo

--- 19-docker-install.sh
+++ 19-docker-install.sh
@@ -6,15 +6,13 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+echo "script stareted executing at $TIMESTAMP" &>>$LOGFILE
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
         echo -e "$2 ... $R FAILED $N"
         exit 1
     else
@@ -22,8 +20,7 @@ VALIDATE(){
     fi
 }
 
-if [ $ID -ne 0 ]
-then
+if [ $ID -ne 0 ]; then
     echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1 # you can give other than 0
 else
@@ -54,7 +51,7 @@ usermod -aG docker centos
 
 VALIDATE $? "added centos user to docker group"
 
-curl -L https://github.com/docker/compose/releases/download/2.24.6/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/2.24.6/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
 
